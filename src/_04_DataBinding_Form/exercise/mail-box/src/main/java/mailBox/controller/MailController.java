8acd0f6fc687b1f7b0caca0cdaa8912a9697d31c
FabulousNBL <nguyenblong2412@gmail.com>
2022-06-17 10:27:57 +0700

    upd

--- src/_04_DataBinding_Form/exercise/mail-box/src/main/java/mailBox/controller/MailController.java
+++ src/_04_DataBinding_Form/exercise/mail-box/src/main/java/mailBox/controller/MailController.java
@@ -12,18 +12,18 @@ public class MailController {
 
 
     @GetMapping
-    public String moveToCreate(Model model){
-        int[] size= new int[] {5,10,15,25,50,100};
-        String[] language= new String[] {"English","Vietnamese","Japanese","chinese"};
-        model.addAttribute("language",language);
-        model.addAttribute("mailBox",new MailBox());
+    public String moveToCreate(Model model) {
+        int[] size = new int[]{5, 10, 15, 25, 50, 100};
+        String[] language = new String[]{"English", "Vietnamese", "Japanese", "chinese"};
+        model.addAttribute("language", language);
+        model.addAttribute("mailBox", new MailBox());
         model.addAttribute("pageSize", size);
         return "create";
     }
 
     @PostMapping("/create")
-    public String create(@ModelAttribute MailBox mailBox, Model model){
-        model.addAttribute("mailBox",mailBox);
+    public String create(@ModelAttribute MailBox mailBox, Model model) {
+        model.addAttribute("mailBox", mailBox);
         return "detail";
     }
 }
